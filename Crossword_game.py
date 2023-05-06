a36d59f63ce7913ffb8e3576e23b2958e03674ae
$ura <80858501+sura-ssd@users.noreply.github.com>
2023-05-06 17:25:21 +0300

    Update from # table to grid

--- Crossword_game.py
+++ Crossword_game.py
@@ -1,29 +1,26 @@
 import tkinter as tk
 from tkinter import messagebox
-def clue_list_to_dict(word_list, clue_list):
-    return {word_list[i]: clue_list[i] for i in range(len(word_list))}
  # Constants for the crossword grid size and the possible words
 GRID_SIZE = 8
 WORD_LIST = ['dog', 'cat', 'bird', 'fish', 'lion', 'tiger', 'owl', 'rat', 'bat', 'cow', 'duck', 'goose', 'frog', 'pig',
              'ant', 'bee', 'hen', 'fox', 'ram']
-CLUE_LIST = clue_list_to_dict(WORD_LIST, ['A pet that barks', 'A pet that meows', 'A creature that flies',
-             'An aquatic animal', 'The king of the jungle', 'A large carnivore with stripes',
-             'A nocturnal bird', 'A small rodent', 'A flying mammal',
-             'A farm animal that gives milk', 'A bird that quacks', 'A large waterbird',
-             'An amphibian that jumps', 'A farm animal that oinks', 'A tiny insect',
-             'An insect that makes honey', 'A female bird that lays eggs',
-             'A cunning, small carnivore', 'A male sheep'])
-grid = [['#' for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
-clue_labels = []
+CLUE_LIST = {'dog': 'A pet that barks', 'cat': 'A pet that meows', 'bird': 'A creature that flies',
+             'fish': 'An aquatic animal', 'lion': 'The king of the jungle', 'tiger': 'A large carnivore with stripes',
+             'owl': 'A nocturnal bird', 'rat': 'A small rodent', 'bat': 'A flying mammal',
+             'cow': 'A farm animal that gives milk', 'duck': 'A bird that quacks', 'goose': 'A large waterbird',
+             'frog': 'An amphibian that jumps', 'pig': 'A farm animal that oinks', 'ant': 'A tiny insect',
+             'bee': 'An insect that makes honey', 'hen': 'A female bird that lays eggs',
+             'fox': 'A cunning, small carnivore', 'ram': 'A male sheep'}
+grid = [[' ' for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
  # Function to update a square in the grid
 def update_grid(row, col, text_var):
     value = text_var.lower()
     grid[row][col] = value
     if value == '':
-        value = '#'
+        value = ' '
     text = '      ' + value + '      '
     labels[row][col].config(text=text)
- # Function to handle submission of a word
+ # Function to handle the submission of a word
 def submit_word():
     word = word_var.get().lower()
     if word not in WORD_LIST:
@@ -37,7 +34,7 @@ def submit_word():
             messagebox.showerror('Error', 'The word does not fit.')
             return
         for i in range(len(word)):
-            if grid[row][col + i] != '#' and grid[row][col + i] != word[i]:
+            if grid[row][col + i] != ' ' and grid[row][col + i] != word[i]:
                 messagebox.showerror('Error', 'The word conflicts with an existing letter.')
                 return
         # Fill in the word horizontally in the grid
@@ -48,16 +45,14 @@ def submit_word():
             messagebox.showerror('Error', 'The word does not fit.')
             return
         for i in range(len(word)):
-            if grid[row + i][col] != '#' and grid[row + i][col] != word[i]:
+            if grid[row + i][col] != ' ' and grid[row + i][col] != word[i]:
                 messagebox.showerror('Error', 'The word conflicts with an existing letter.')
                 return
         # Fill in the word vertically in the grid
         for i in range(len(word)):
             update_grid(row + i, col, word[i])
-     # Make the corresponding clue label's background green
-    for lbl in clue_labels:
-        if lbl["text"].split(". ")[1] == CLUE_LIST[word]:
-            lbl.config(bg="green")
+     # Change the clue label background color to green
+    clue_labels[word].config(bg='green')
      # Clear the entry fields after successful submission
     word_var.set('')
     direction_var.set('')
@@ -66,7 +61,7 @@ def submit_word():
  # Create the Tkinter GUI
 root = tk.Tk()
 root.title('Crossword Game')
-# Create the crossword grid using labels
+ # Create the crossword grid using labels
 labels = []
 square_vars = []
 for i in range(GRID_SIZE):
@@ -76,21 +71,22 @@ for i in range(GRID_SIZE):
         var = tk.StringVar()
         var.trace('w', lambda *args, row=i, col=j, var=var: update_grid(row, col, var))
         var_row.append(var)
-        label = tk.Label(root, text='                  #                    ')
+        label = tk.Label(root, text='                 ', bg="white", relief="groove")
         label.grid(row=i + 1, column=j)
         row.append(label)
     labels.append(row)
     square_vars.append(var_row)
-# Create the clue labels
+ # Create the clue labels
 clue_label = tk.Label(root, text='Clues:', font=('Arial', 14, 'bold'))
 clue_label.grid(row=0, column=GRID_SIZE + 1)
+clue_labels = {}
 clue_number = 1
 for word, clue in CLUE_LIST.items():
     label = tk.Label(root, text=f'{clue_number}. {clue}')
     label.grid(row=clue_number, column=GRID_SIZE + 1)
-    clue_labels.append(label)
+    clue_labels[word] = label
     clue_number += 1
-# Create the word submission fields
+ # Create the word submission fields
 word_label = tk.Label(root, text='Word:', font=('Arial', 12))
 word_label.grid(row=GRID_SIZE + 2, column=0)
 word_var = tk.StringVar()
@@ -113,5 +109,5 @@ col_entry = tk.Entry(root, textvariable=col_var)
 col_entry.grid(row=GRID_SIZE + 5, column=1)
 submit_button = tk.Button(root, text='Submit', command=submit_word)
 submit_button.grid(row=GRID_SIZE + 6, column=1)
-# Start the GUI mainloop
+ # Start the GUI mainloop
 root.mainloop()
